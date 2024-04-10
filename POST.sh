47c4b7cedfc69bd69c5ce31782f0237ad1e6acdf
Yahan Chen <chen2254@wisc.edu>
2024-04-10 12:15:22 -0500

    add code in POST.sh to create countsOfWords

--- POST.sh
+++ POST.sh
@@ -5,3 +5,11 @@ output_file="merged_sorted_file.txt"
 sort -m shakespeare_*.out > "$output_file"
 
 echo "Merged and sorted files into $output_file"
+
+if [[ -e "merged_sorted_file.txt" ]]; then
+    uniq -c merged_sorted_file.txt > countsOfWords
+fi
+sort -nr countsOfWords > sorted_countsOfWords
+mv sorted_countsOfWords countsOfWords
+
+echo "countsOfWords is created"
